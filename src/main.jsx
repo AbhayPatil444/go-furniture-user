3106816e23dc2b4a1b2c63d1d7d33d4f9fe65ad1
AbhayPatil-005 <abhay.official005@gmail.com>
2026-02-25 15:17:23 +0530

    Installed React-Toastify package

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -8,12 +9,23 @@ import {Provider} from 'react-redux';
 import store from './reduxStore/store.js';
 
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify/unstyled';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <ToastContainer
+          position="top-center"
+          autoClose={2500}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme="colored"
+          />
       </BrowserRouter>
     </Provider>
   </StrictMode>,
